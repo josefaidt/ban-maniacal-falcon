3a17c5bee40e1a89ad8bdc9096b278078f0c918f
josefaidt <josef.aidt@gmail.com>
2022-12-28 11:51:30 -0800

    Created a new Plasmo extension

---
+++ contents/plasmo-inline.svelte
@@ -0,0 +1,31 @@
+<script context="module" lang="ts">
+  import type {
+    PlasmoContentScript,
+    PlasmoGetInlineAnchor,
+    PlasmoMountShadowHost
+  } from "plasmo"
+
+  export const config: PlasmoContentScript = {
+    matches: ["https://www.plasmo.com/*"]
+  }
+
+  export const getInlineAnchor: PlasmoGetInlineAnchor = () =>
+    document.querySelector("#supercharge > h2 > span")
+
+  export const mountShadowHost: PlasmoMountShadowHost = ({
+    inlineAnchor,
+    shadowHost
+  }) => {
+    inlineAnchor.insertBefore(shadowHost, inlineAnchor.firstChild)
+  }
+</script>
+
+<script>
+  let count = 0
+  let increment = () => count++
+</script>
+
+<div>
+  <span style="color: red;">{count}</span>
+  <button on:click={increment}>A lie is a very poor way to say hello</button>
+</div>
